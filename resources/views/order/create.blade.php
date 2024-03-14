d0b4da5d60bfdb36ab29b95d29e00ee8a9e8b506
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-03-14 23:59:03 +0500

    create blade update

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -4,7 +4,14 @@
             Navbatga yozilish
         </h2>
     </x-slot>
+    {{--    @dd($errors->all())--}}
+        @foreach($errors->all() as $error)
+    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
 
+            <strong class="font-bold">Xatolik!</strong>
+            <span class="block sm:inline">{{ $error }}</span>
+    </div>
+    @endforeach
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -18,18 +25,28 @@
                             <label class="block text-gray-700 text-sm font-bold mb-2" for="from">
                                 Boshlanish vaqti
                             </label>
-                            <input required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="from" type="datetime-local" name="from">
+                            <input required
+                                   class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                   id="from" type="datetime-local" name="from">
                         </div>
                         <div class="mb-4">
                             <label class="block text-gray-700 text-sm font-bold mb-2" for="to">
                                 Tugash vaqti
                             </label>
-                            <input required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="to" type="datetime-local" name="to">
+                            <input required
+                                   class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                   id="to" type="datetime-local" name="to">
                         </div>
                         <div class="flex items-center justify-between">
-                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Saqlash</button>
-                            <a href="{{ route('site.showNavbat', $moyka->id) }}" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Orqaga</a>
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                    type="submit">Saqlash
+                            </button>
+
+                            <a href="{{ route('site.showNavbat', $moyka->id) }}"
+                               class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Orqaga</a>
                         </div>
+
+                        <input type="hidden" value="{{$moyka->id}}" name="moyka_id">
                     </form>
 
 
