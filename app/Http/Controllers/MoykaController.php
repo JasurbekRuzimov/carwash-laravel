3d7b6935b9625c430619f28394d587c4d00919a6
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-03-14 23:59:44 +0500

    update

--- app/Http/Controllers/MoykaController.php
+++ app/Http/Controllers/MoykaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\MoykaRequest;
 use App\Models\Moyka;
 use Illuminate\Http\Request;
 
@@ -56,7 +57,7 @@ class MoykaController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(MoykaRequest $request, string $id)
     {
         $m = Moyka::find($id);
         $m->name = $request->name;
